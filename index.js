03853988da72720b1601c6e039aba1d304808f91
Jack C <jkchin2@buffalo.edu>
2025-08-12 22:12:39 -0400

    done

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.all("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
